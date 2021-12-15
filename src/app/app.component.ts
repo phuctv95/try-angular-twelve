79d38cd30ee433234f986b7f021866fce852bf25
Tran Van Phuc <prv@elca.vn>
2021-12-15 14:46:24 +0700

    angular material: get random cat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'try-angular-twelve';
+  imgSrc = null;
+  isGettingCat = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getRandomCatPhoto();
+  }
+
+  getRandomCatPhoto() {
+    this.isGettingCat = true;
+    this.http
+      .get<any>('https://aws.random.cat/meow')
+      .subscribe(res => {
+        this.imgSrc = res.file;
+      });
+  }
+
+  onLoadImgCompleted() {
+    this.isGettingCat = false;
+  }
 }
